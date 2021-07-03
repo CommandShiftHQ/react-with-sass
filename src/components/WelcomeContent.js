c91be6803084383c8974c7f0b1a008ddcb9bef73
StuartCowley <voidstalker@live.co.uk>
2021-07-03 13:41:11 +0100

    scaffold out basic structure, add router, cleanup

---
+++ src/components/WelcomeContent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+import ResetPasswordForm from "./ResetPasswordForm";
+import common from "../config/common";
+
+const WelcomeContent = () => {
+  return (
+    <div className="welcome-content">
+      <img
+        src={common.logoImage}
+        alt="we are ManchesterCodes"
+        className="logo"
+      />
+      <Switch>
+        <Route exact path="/">
+          <RegisterForm />
+        </Route>
+        <Route path="/login">
+          <LoginForm />
+        </Route>
+        <Route path="/reset-password">
+          <ResetPasswordForm />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default WelcomeContent;
